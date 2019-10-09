b15cd477d70045b3abeef42a1e86b032dc25175d
priyanka08dalal <priyanka.RajnishbhaiDalal@dal.ca>
2019-10-09 10:00:35 -0700

    multiples of both three and five print “FizzBuzz

---
+++ task0.py
@@ -0,0 +1,10 @@
+string = []
+for i in range(1,100)
+	if i%3==0
+		string = string+"Fizz"
+	elif i%5==0
+		string=string+"Buzz"
+	elif i%3==0 and i%5==0
+		string=string+"Fizzbuzz"
+	else
+		string=string+str(num)
